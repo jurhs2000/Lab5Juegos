d803e145f3d6c7d0550023555893243250db2c91
Julio Herrera <jurhs2000@gmail.com>
2020-05-02 07:11:58 -0600

    UI slider de control de volumen en scene 2

--- Assets/Scripts/PlayerSound.cs
+++ Assets/Scripts/PlayerSound.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class PlayerSound : MonoBehaviour
 {
 
-    private CharacterController m_CharacterController;
+    private Rigidbody m_CharacterController;
     private AudioSource m_AudioSource;
     [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+    [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+    private float m_StepCycle;
+    private float m_NextStep;
+    private bool m_PreviouslyGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_CharacterController = GetComponent<Rigidbody>();
+        m_StepCycle = 0f;
+        m_NextStep = m_StepCycle / 2f;
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (!m_PreviouslyGrounded && m_CharacterController)
+        {
+            PlayLandingSound();
+        }
     }
 
     private void FixedUpdate()
@@ -31,10 +42,16 @@ public class PlayerSound : MonoBehaviour
         PlayFootStepAudio();
     }
 
+    private void PlayLandingSound()
+    {
+        m_AudioSource.clip = m_LandSound;
+        m_AudioSource.Play();
+        m_NextStep = m_StepCycle + .5f;
+    }
 
     private void PlayFootStepAudio()
     {
-        if (!m_CharacterController.isGrounded)
+        if (!m_CharacterController)
         {
             return;
         }
---
+++ Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider = GetComponent<Slider>();
+        Debug.Log(slider.value);
+        FindObjectOfType<AudioManager>().setVolume("back", slider.value);
+    }
+
+}
